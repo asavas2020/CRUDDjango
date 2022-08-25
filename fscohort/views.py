73c3b80905e40f608c963d694159723067a81072
Gokhan <gknsvsdata@gmail.com>
2022-08-25 22:39:36 +0300

    update

--- fscohort/views.py
+++ fscohort/views.py
@@ -29,5 +29,27 @@ def student_add(request):
 
     return render(request, "fscohort/student_add.html", context)
 
-def student_update(request):
-    
+def student_update(request, id):
+    student = Student.objects.get(id = id)
+    form = StudentForm(instance = student)
+    if request.method == "post":
+        form = StudentForm(request.POST, instance = student)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+
+    context= {
+        "form" : form
+    }
+
+    return render(request, "fscohort/student_update.html", context)
+
+def student_delete(request,id):
+    student = Student.objects.get(id = id)
+    if request.method == "post":
+        student.delete()
+        return redirect("list")
+
+    return render(request, "fscohort/student_delete.html")
+
+
