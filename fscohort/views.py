c83e41c8e19eba0eb9b62f07c062e1e8e70f9b91
Gokhan <gknsvsdata@gmail.com>
2022-08-25 21:45:29 +0300

    read

---
+++ fscohort/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Student
+
+# Create your views here.
+def index(request):
+    return render(request, "fscohort/index.html")
+
+def student_list(request):
+    students=Student.objects.all()
+    context = {
+        "students" : students
+    }
+
+    return render(request, "fscohort/student_list.html", context)
+    # return render(request, "fscohort/student_list.html", { "students" : students})
+
